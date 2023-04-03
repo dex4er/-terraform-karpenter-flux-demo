b5183bd19efcf4adade2dfa62bdd04303753ad13
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-04-03 15:37:02 +0200

    Add node type as a suffix for role name

--- flux/eks-node-role-label/files/entrypoint.sh
+++ flux/eks-node-role-label/files/entrypoint.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 shopt -s inherit_errexit 2>/dev/null || true
 
 while true; do
-  kubectl get node -L eks.amazonaws.com/nodegroup,karpenter.sh/provisioner-name |
-    while read -r name _status roles _age _version nodegroup provisioner; do
+  kubectl get node -L node.kubernetes.io/instance-type,eks.amazonaws.com/nodegroup,karpenter.sh/provisioner-name |
+    while read -r name _status roles _age _version type nodegroup provisioner; do
       if [[ ${roles} == "<none>" ]]; then
         if [[ -n ${nodegroup} ]]; then
-          kubectl label node ${name} node-role.kubernetes.io/${nodegroup}=
+          kubectl label node ${name} node-role.kubernetes.io/${nodegroup}.${type}=
         elif [[ -n ${provisioner} ]]; then
-          kubectl label node ${name} node-role.kubernetes.io/${provisioner}=
+          kubectl label node ${name} node-role.kubernetes.io/${provisioner}.${type}=
         fi
       fi
     done
